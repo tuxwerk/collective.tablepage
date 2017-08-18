19969989cd95dfd82dd668463da8e01562c94b27
Marek Kralewski <mck@tuxwerk.de>
2017-08-18 13:56:09 +0200

    fix for multiple-tables-view

--- collective/tablepage/browser/multiple_tables.py
+++ collective/tablepage/browser/multiple_tables.py
@@ -10,7 +10,12 @@ class MultipleTablesView(TableViewView):
         return self.index()
 
     def is_label(self, row):
-        return isinstance(row, basestring) and row or None
+        if row == 0:
+            # Where does the 0 come from???
+            return None
+        if '__label__' in row.keys():
+            return row['__label__']
+        return None
 
     def getHeader(self, col_index):
         return self.headers()[col_index]
